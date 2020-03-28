b0299c7293fb92f12b61eb47954082e2af1cc768
Leo <icpc0928@gmail.com>
2020-03-28 14:39:22 +0800

    1

---
+++ app/src/main/java/tw/org/iii/leo/leo21/MainActivity.java
@@ -0,0 +1,36 @@
+package tw.org.iii.leo.leo21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webView);
+
+        initWebView();
+    }
+
+    private void initWebView(){
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.iii.org.tw");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
